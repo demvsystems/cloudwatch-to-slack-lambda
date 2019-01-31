d78e632aafb0d10608d3639ff2ff50436145c6d9
snowiow <marcel.patzwahl+github@posteo.de>
2019-01-31 11:13:42 +0100

    Clippy added

--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,10 @@ impl LambdaErrorExt for CustomError {
 
 fn get_log_level() -> log::Level {
     use std::env;
-    match env::var("LOG_LEVEL").unwrap_or("info".to_string()).as_ref() {
+    match env::var("LOG_LEVEL")
+        .unwrap_or_else(|_| "info".to_string())
+        .as_ref()
+    {
         "trace" => log::Level::Trace,
         "error" => log::Level::Error,
         "debug" => log::Level::Debug,
@@ -55,7 +58,7 @@ fn send_slack_msg(msg: &str) -> Result<(), CustomError> {
 
     let webhook = env::var("SLACK_WEBHOOK").unwrap();
     let channel_name = env::var("CHANNEL_NAME").unwrap();
-    let username = env::var("USERNAME").unwrap_or("SnsToSlackLambda".to_string());
+    let username = env::var("USERNAME").unwrap_or_else(|_| "SnsToSlackLambda".to_string());
     let slack = Slack::new(webhook.as_str()).unwrap();
     let p = PayloadBuilder::new()
         .text(msg)
